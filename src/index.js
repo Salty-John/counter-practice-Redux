bb4d3b30ad11f3f9173cb832de9a7e2509b4c67d
Salty-John <36969449+Salty-John@users.noreply.github.com>
2018-05-16 14:01:32 -0700

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { render } from 'react-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import App from './components/App';
+import counter from './reducers';
+
+//create Store
+const store = createStore(counter);
+
+render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('app')
+);

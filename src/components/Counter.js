b8211a5c66f957c78a8bcd95715cc1bec473d710
Salty-John <36969449+Salty-John@users.noreply.github.com>
2018-05-16 14:25:19 -0700

    Create Counter.js

---
+++ src/components/Counter.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import * as actions from '../actions';
+
+//function increment(sta)
+
+class Counter extends React.Component {
+  render() {
+    const {increment, decrement, count } = this.props;
+      return (
+        <div>
+          Current counter value: {count}
+          <div><button onClick={(e) => increment()}>+</button></div>
+          <div><button onClick={(e) => decrement()}>-</button></div>
+        </div>
+      );
+   }
+}
+
+const mapStateToProps = (state) => {
+  return {
+    count: state
+  }
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return bindActionCreators(actions, dispatch)
+}
+export default connect(mapStateToProps, mapDispatchToProps)(Counter);

9ee4a96fb14d75743d044a5f63e13274324d14bd
Salty-John <36969449+Salty-John@users.noreply.github.com>
2018-05-16 14:03:09 -0700

    Create App.js

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+mport React from 'react';
+import Counter from '../components/Counter';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <Counter />
+      </div>
+    );
+  }
+}
+
+export default App;
